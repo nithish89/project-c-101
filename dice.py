75ea74690ec2e54fe95195a20bb7e7a06ef8ffd2
nithish89 <78968812+nithish89@users.noreply.github.com>
2024-02-14 14:03:49 +0000

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,54 @@
+import random
+def response(num):
+
+    while response == "y":
+    
+        num = random.randint(1,6)
+
+    if num == 1:
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")
+
+    if num == 2:
+        print("[-----]")
+        print("[     ]")
+        print("[  1  ]")
+        print("[     ]")
+        print("[-----]")
+    if num == 3:
+        print("[-----]")
+        print("[     ]")
+        print("[  2  ]")
+        print("[     ]")
+        print("[-----]")
+
+    if num == 4:
+        print("[-----]")
+        print("[     ]")
+        print("[  3  ]")
+        print("[     ]")
+        print("[-----]")
+
+    if num == 5:
+        print("[-----]")
+        print("[     ]")
+        print("[  4  ]")
+        print("[     ]")
+        print("[-----]")
+    
+    if num == 6:
+        print("[-----]")
+        print("[     ]")
+        print("[  5  ]")
+        print("[     ]")
+        print("[-----]")
+
+    if num == 7:
+        print("[-----]")
+        print("[     ]")
+        print("[  6  ]")
+        print("[     ]")
+        print("[-----]")
